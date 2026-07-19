c868516a170eab4268ff908babd373adbd1f200a
froxtjack <hmkcqog910@flutterstack.dev>
2026-07-19 18:03:49 +0530

    Update scraper_bot.py

--- scraper_bot.py
+++ scraper_bot.py
@@ -1,4 +1,4 @@
-import os
+8import os
 import random
 import time
 import asyncio
@@ -291,7 +291,7 @@ async def send_approved_card(chat_id, card_data=None):
 🌎 <b>𝗖𝗶𝘁𝘆</b> {card_data['country']} {card_data['country_flag']}
 ━━━━━━━━━━━━━━━━━━━━━━━━
 ⭐ <b>@{'Scra07bot'}</b>
-{datetime.now().strftime('%I:%M %p')}"""
+{datetime.now().strftime('%I:%M %p')}"""}
     
     buttons = [
         [Button.inline("𝚅𝙸𝙿", b"vip", style="success"), Button.inline("𝙲𝙷𝙰𝚁𝙶𝙴", b"charge", style="success"), 
